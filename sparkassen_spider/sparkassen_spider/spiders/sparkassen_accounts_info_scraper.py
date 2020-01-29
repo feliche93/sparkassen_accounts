f5bf358725157213ec0803842bbbcf8e5a94d946
Felix Vemmer <felix.vemmer@gmail.com>
2020-01-29 23:58:13 +0100

    finished scraping properly account details link

--- sparkassen_spider/sparkassen_spider/spiders/sparkassen_accounts_info_scraper.py
+++ sparkassen_spider/sparkassen_spider/spiders/sparkassen_accounts_info_scraper.py
@@ -24,13 +24,45 @@ class SparkassenAccountsScraper(scrapy.Spider):
         accounts = response.xpath(
             '//div[@class="cbox cbox-product cbox-small section"]')
 
+        # Second site script to find accounts
         if len(accounts) == 0:
             accounts = response.xpath(
                 '//div[contains(@class, "cbox-image")]')
 
+        for account in accounts:
+            base_url = response.url.split('.de/')[0] + '.de'
+
+            if account.xpath('.//a/@href').extract_first() and account.xpath('.//span/text()').extract_first():
+                if re.search(r"(Konto|konto|giro|Giro)", account.xpath('.//span/text()').extract_first()):
+                    account_name = account.xpath(
+                        './/span/text()').extract_first()
+                    account_detail_url = base_url + account.xpath(
+                        './/a/@href').extract_first()
+
+                    yield scrapy.Request(url=account_detail_url, callback=self.parse_account_details, meta={'account_name': account_name, 'account_detail_url': account_detail_url, 'requested_url': response.url})
+
+            elif account.xpath('.//a/@href').extract_first() and account.xpath('.//h2/text()').extract_first():
+                if re.search(r"(Konto|konto|giro|Giro)", account.xpath('.//h2/text()').extract_first()):
+                    account_name = account.xpath(
+                        './/h2/text()').extract_first()
+                    account_detail_url = base_url + account.xpath(
+                        './/a/@href').extract_first()
+
+                    yield scrapy.Request(url=account_detail_url, callback=self.parse_account_details, meta={'account_name': account_name, 'account_detail_url': account_detail_url, 'requested_url': response.url})
+
+            else:
+                print(response.url)
+
+    def parse_account_details(self, response):
+
+        tables = response.xpath('//table')
+        print(len(tables))
+
         yield {
-            'requested_url': response.url,
-            'tables_on_website': len(accounts)
+            'account_name': response.meta['account_name'],
+            'account_detail_url': response.meta['account_detail_url'],
+            'requested_url': response.meta['requested_url'],
+            'tables_on_website': len(tables)
         }
 
 
